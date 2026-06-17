d7386ba38e12390da5b63062d3ca6ef89016dfea
Thusantha-Lakshitha <thusanthabandara41@gmail.com>
2026-06-18 00:14:08 +0530

    feat: Connect MongoDB Atlas database as standard fallback

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/online-shopping');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://stores:QcsDO2UIyuHU35Dx@cluster0.eagm2k2.mongodb.net/online_store?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
